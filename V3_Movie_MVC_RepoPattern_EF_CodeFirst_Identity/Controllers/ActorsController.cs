cc815b91ec4efcf3a7a1ae496f5bca82e1d55241
HP <you@example.com>
2019-11-01 15:28:37 +0530

    Add project files.

---
+++ V3_Movie_MVC_RepoPattern_EF_CodeFirst_Identity/Controllers/ActorsController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using V3_Movie_MVC_RepoPattern_EF_CodeFirst_Identity.Data;
+using V3_Movie_MVC_RepoPattern_EF_CodeFirst_Identity.Models;
+
+namespace V3_Movie_MVC_RepoPattern_EF_CodeFirst_Identity.Controllers
+{
+    public class ActorsController : Controller
+    {
+        IRepository repository;
+        public ActorsController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        // GET: Actors
+        public ActionResult Index()
+        {
+            return View(repository.GetMovies());
+        }
+
+        // GET: Actors/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Actors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Actors/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Actor actor )
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    bool result = repository.AddActor(actor);
+                    if (result)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                    
+                        return View();
+                    
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Actors/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Actors/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Actors/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Actors/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ V3_Movie_MVC_RepoPattern_EF_CodeFirst_Identity/Data/MovieRepository.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using V3_Movie_MVC_RepoPattern_EF_CodeFirst_Identity.Models;
+
+namespace V3_Movie_MVC_RepoPattern_EF_CodeFirst_Identity.Data
+{
+    public class MovieRepository : IRepository
+    {
+        ApplicationDbContext context = null;
+        public MovieRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public bool AddActor(Actor actor)
+        {
+            try
+            {
+                context.Actors.Add(actor);
+                int result = context.SaveChanges();
+                if (result == 1)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool AddMovie(Movie movie)
+        {
+            try
+            {
+                context.Movies.Add(movie);
+                int result = context.SaveChanges();
+                if (result == 1)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool DeleteActor(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditActor(Actor actor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EditMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetActorById(int ActorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Actor> GetActors()
+        {
+            return context.Actors.ToList();
+        }
+
+        public IEnumerable<Actor> GetActorsByMovie(int movieId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Actor> GetActorsByGenre(Gender gender)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetMovieById(int movieId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return context.Movies.ToList();
+        }
+
+        public IEnumerable<Movie> GetMoviesByActor(int actorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Movie> GetMoviesByGenre(Genre genre)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
